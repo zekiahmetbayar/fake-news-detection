5428a6648b8789cd94a3c49b57b17c9855521b2d
Zeki Ahmet Bayar <54356667+zekiahmetbayar@users.noreply.github.com>
2021-01-04 03:25:01 +0300

    Delete add_label.py

--- add_label.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-df=pd.read_csv("/home/tubi/Desktop/DetectFakeTrueNews/KullanilacakVeriler/LabelsizVeri/dogru.csv")
-df["Label"]=1
-df.to_csv("/home/tubi/Desktop/DetectFakeTrueNews/KullanilacakVeriler/LabelliVeri/dogru1.csv",index=False)
